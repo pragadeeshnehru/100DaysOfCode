63103f7526a620c22bc1dc691d54840b77e31c52
Pragadeesh Nehru <pragadeeshxenocrate@gmail.com>
2024-07-22 22:18:22 +0530

    Dice stimulator using python

---
+++ Day 2 - Dice Generator/dice_generator.py
@@ -0,0 +1,27 @@
+import tkinter
+import tkinter.messagebox
+import random
+from PIL import Image, ImageTk
+
+window = tkinter.Tk()
+window.geometry('240x240')
+
+min = 1
+max = 6
+
+def diceResult():
+    diceOutput = str(random.randint(min, max))
+    print(' Dice Value ', diceOutput)
+    button['text'] = 'Dice Rolled ' + diceOutput
+    imageFile = f"Dice{diceOutput}.png"
+    
+    # Resize the image to fit the window size
+    img = Image.open(imageFile)
+    img = img.resize((240, 240), Image.LANCZOS)
+    window.photo1 = ImageTk.PhotoImage(img)
+    button['image'] = window.photo1
+
+button = tkinter.Button(window, text="Click to roll the dice", command=diceResult)
+button.pack()
+window.title('Digital Dice')
+window.mainloop()
